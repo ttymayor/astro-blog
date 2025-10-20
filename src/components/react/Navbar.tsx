a97fd8ece894a013efb109704dc36166d7e023cb
ttymayor <lionhuang914903@gmail.com>
2025-10-20 14:39:32 +0800

    feat: implement Drawer component in Navbar for mobile menu accessibility and improved user experience

--- src/components/react/Navbar.tsx
+++ src/components/react/Navbar.tsx
@@ -4,6 +4,16 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
 import { useState, useEffect } from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 interface NavbarProps {
   menu: {
@@ -28,13 +38,36 @@ export default function Navbar({ menu, pathname }: NavbarProps) {
   }, []);
 
   return isMobile ? (
-    <div className="flex items-center gap-4">
-      <Button
-        variant="ghost"
-        className="bg-background cursor-pointer border-0 text-base"
-      >
-        <MenuIcon className="size-4" />
-      </Button>
+    <div className="flex items-center justify-center gap-4">
+      <Drawer direction="top">
+        <DrawerTrigger className="bg-background flex cursor-pointer items-center justify-center gap-4 border-0 text-base">
+          <MenuIcon className="size-6" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Menu</DrawerTitle>
+            <DrawerDescription>
+              <div className="flex flex-col gap-2">
+                {menu.map((link) => (
+                  <a
+                    href={link.href}
+                    className=""
+                    data-astro-prefetch
+                    key={link.label}
+                  >
+                    <Button
+                      variant="ghost"
+                      className="bg-background w-full cursor-pointer border-0 text-base"
+                    >
+                      {link.label}
+                    </Button>
+                  </a>
+                ))}
+              </div>
+            </DrawerDescription>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
     </div>
   ) : (
     <nav className="[&>a]:text-foreground [&>a]:hover:text-primary flex items-center gap-4 [&>a]:text-sm [&>a]:font-medium">
