df4f63cf240f55db320c70b06ab9008f0fe4c06c
ttymayor <lionhuang914903@gmail.com>
2025-10-25 02:37:42 +0800

    feat: enhance SiteCounter component with animation effects and update footer to include SiteCounter

--- src/components/react/SiteCounter.tsx
+++ src/components/react/SiteCounter.tsx
@@ -1,16 +1,23 @@
 import { useVercount } from "vercount-react";
 import CountUp from "@/components/react/CountUp";
+import { motion } from "motion/react";
 
 export default function SiteCounter() {
   const { sitePv, siteUv } = useVercount();
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
+    <motion.div
+      className="flex flex-col items-center justify-center gap-2"
+      initial={{ opacity: 0, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+    >
       <p className="mx-auto my-0">
         Site Page Views: <CountUp to={Number(sitePv)} duration={1} />
       </p>
       <p className="mx-auto my-0">
         Unique Visitors: <CountUp to={Number(siteUv)} duration={1} />
       </p>
-    </div>
+    </motion.div>
   );
 }
